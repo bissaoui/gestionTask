d3f01f6c0ed75f8e41ab93c729f9bf72d3e1815f
bissaoui <yassine.bissaoui@gmail.com>
2024-09-18 16:27:19 +0100

    first commit

---
+++ src/main/java/com/gestionTaches/GestionDesTachesApplication.java
@@ -0,0 +1,13 @@
+package com.gestionTaches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesTachesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesTachesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionTaches/GestionDesTachesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionTaches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesTachesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
